31cc7b9a005e9762813b1540499afec9c582fe24
TuubaCa <tuubacakara@gmail.com>
2024-06-16 22:14:31 +0200

    first commit

---
+++ src/test/java/tests/C01_LoginTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+
+import org.testng.annotations.Test;
+import pages.CLContactPage;
+import pages.CLHomePage;
+import utilities.ConfigReader;
+import utilities.Driver;
+
+import static org.testng.Assert.assertEquals;
+
+public class C01_LoginTest {
+
+    @Test
+    public void loginTest01() throws InterruptedException {
+
+        CLHomePage clHomePage = new CLHomePage();
+        CLContactPage clContactPage = new CLContactPage();
+
+        //Go to url
+        Driver.getDriver().get(ConfigReader.getProperty("contact_list_url"));
+
+        //Enter email
+        clHomePage.email.sendKeys("clarusway@hotmail.com");
+
+        //Enter password
+        clHomePage.password.sendKeys("Clarusway.123");
+
+        //Click submit
+        clHomePage.submit.click();
+        Thread.sleep(200);
+
+        //Assert Contact List Header
+        assertEquals(clContactPage.header.getText(), "Contact List");
+        assert clContactPage.logout.isDisplayed();
+
+        Driver.closeDriver();
+    }
+
+
+}
